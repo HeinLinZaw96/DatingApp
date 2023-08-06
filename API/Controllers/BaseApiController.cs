09fac0c37d34c94062ead25c36f65ad738ce5719
Hein Lin Zaw <e0941685@u.nus.edu>
2023-08-06 21:10:07 +0800

    End of section 4

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]     // api/baseapi
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
